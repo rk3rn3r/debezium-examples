1a08c1f0a0e325395654feb8162334b7483d05d2
rkerner <rkerner.mobil@gmail.com>
2020-07-20 10:38:48 +0200

    TASKLESS add example automation PoC WIP

---
+++ tutorial/scripts/tutorial_mysql_check-debezium-avro.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+docker-compose -f docker-compose-mysql-avro-worker.yaml exec schema-registry /usr/bin/kafka-avro-console-consumer \
+    --bootstrap-server kafka:9092 \
+    --from-beginning \
+    --property print.key=true \
+    --property schema.registry.url=http://schema-registry:8081 \
+    --topic dbserver1.inventory.customers
---
+++ tutorial/scripts/tutorial_mysql_check-debezium-json.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+docker-compose -f docker-compose-mysql-apicurio.yaml exec kafka /kafka/bin/kafka-console-consumer.sh \
+    --bootstrap-server kafka:9092 \
+    --from-beginning \
+    --property print.key=true \
+    --topic dbserver1.inventory.customers
